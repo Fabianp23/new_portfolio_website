a97b92a0ef29c594f572abab3d0f27790e214635
Fabian Pombo <contact@fabianpombo.com>
2016-06-02 11:22:59 -0400

    added landing page animation to logo and sub-logos

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 
-  $(".dropdown a:first-of-type").on('mouseenter', function(){
+  $(".dropdown a:first-of-type").on('mouseenter', function() {
     $("#dropdown-work").show('slow');
   });
 
-  $("#dropdown-work").on('mouseleave', function(){
+  $("#dropdown-work").on('mouseleave', function() {
     $(this).hide('slow');
+  });
+
+  $("#logo-image").hide('load', function() {
+    $(this).fadeIn(3000);
+  });
+
+  $("#graphic-design").hide('load', function() {
+    $(this).animate({left:225, opacity:"show"}, 1500);
+  });
+
+  $("#front-end").hide('load', function() {
+    $(this).animate({left:250, opacity:"show"}, 1500);
   });
 
 });
