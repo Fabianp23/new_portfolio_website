fce494e481e3fe7c4ad19795493d9e2a3337b648
Fabian Pombo <contact@fabianpombo.com>
2016-06-02 14:13:59 -0400

    added animation to icons on index page using js

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
 
-  $(".dropdown a:first-of-type").on('mouseenter', function() {
-    $("#dropdown-work").show('slow');
+  $('.dropdown a:first-of-type').on('mouseenter', function() {
+    $('#dropdown-work').show('slow');
   });
 
-  $("#dropdown-work").on('mouseleave', function() {
+  $('#dropdown-work').on('mouseleave', function() {
     $(this).hide('slow');
   });
 
-  $("#logo-image").hide('load', function() {
+  $('#logo-image').hide('load', function() {
     $(this).fadeIn(3000);
   });
 
-  $("#graphic-design").hide('load', function() {
-    $(this).animate({left:225, opacity:"show"}, 1500);
+  $('#graphic-design').hide('load', function() {
+    $(this).animate({left:200, opacity:'show'}, 1500);
   });
 
-  $("#front-end").hide('load', function() {
-    $(this).animate({left:250, opacity:"show"}, 1500);
+  $('#front-end').hide('load', function() {
+    $(this).animate({left:225, opacity:'show'}, 1500);
   });
+
+
+  function expandImg(img) {
+    $(img).on('mouseenter', function() {
+      $(this).css('padding', '5px');
+    });
+    $(img).on('mouseleave', function() {
+      $(this).css('padding', '0px');
+    });
+  };
+
+  expandImg('#logo-image');
+  expandImg('#graphic-design');
+  expandImg('#front-end');
 
 });
