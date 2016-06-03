e21b2d233eb139a61b084a26b14a25b0cb814992
Fabian Pombo <contact@fabianpombo.com>
2016-06-03 14:47:37 -0400

    removed unnecessary div in nav bar. fixed an issue where margins weren't adjusting when re-sizing. Changed background image to crop on resize

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
     });
   };
 
-  $('.dropdown a:first-of-type').on('mouseenter', function() {
+  $('.work').on('mouseenter', function() {
     $('#dropdown-work').show('slow');
   });
 
