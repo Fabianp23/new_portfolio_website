36f1943b15e5e377377d5444117ddd0f3138fd7b
Fabian Pombo <contact@fabianpombo.com>
2016-07-05 17:59:26 -0400

    added animsition jquery plugin for page transitions

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -94,4 +94,26 @@ $(document).ready(function(){
   showHidden('.contact-linkedin');
   showHidden('.contact-github');
 
+// Animsition //
+
+  $(".animsition").animsition({
+    inClass: 'fade-in-down',
+    outClass: 'fade-out-up',
+    inDuration: 1000,
+    outDuration: 2000,
+    linkElement: '.animsition-link',
+    loading: true,
+    loadingParentElement: 'body',
+    loadingClass: 'animsition-loading',
+    loadingInner: '',
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+
 });
